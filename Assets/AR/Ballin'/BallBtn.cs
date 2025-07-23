7271a1264ab4962d6dab3f8c3cb8486119bf7c69
eneumont <esaintpierre@student.neumont.edu>
2025-07-23 13:03:16 -0600

    a

--- Assets/AR/Ballin'/BallBtn.cs
+++ Assets/AR/Ballin'/BallBtn.cs
@@ -1,12 +1,15 @@
 using Unity.XR.CoreUtils;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit.Inputs;
 
 public class BallBtn : MonoBehaviour {
     [SerializeField] private XROrigin xrOrigin;
+    [SerializeField] private InputActionManager inputManager;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start() {
 		xrOrigin ??= GetComponent<XROrigin>();
+        inputManager ??= GetComponent<InputActionManager>();
 	}
 
     // Update is called once per frame
