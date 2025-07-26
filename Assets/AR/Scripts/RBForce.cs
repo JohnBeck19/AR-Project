8c357e7eb43d9e0f1834726a34428f8c4fb7be5e
eneumont <esaintpierre@student.neumont.edu>
2025-07-26 14:32:00 -0600

    inital basketball setup
    base stuff implemented just need to clean some stuff up

--- Assets/AR/Scripts/RBForce.cs
+++ Assets/AR/Scripts/RBForce.cs
@@ -1,11 +1,9 @@
 using UnityEngine;
 
-public class RBForce : MonoBehaviour
-{
-    [SerializeField] float force;
+public class RBForce : MonoBehaviour {
+    [SerializeField] public float force;
 
-    void Start()
-    {
+    void Start() {
         Rigidbody rb = GetComponent<Rigidbody>();
         rb.AddRelativeForce(Vector3.forward * force, ForceMode.VelocityChange);
     }
